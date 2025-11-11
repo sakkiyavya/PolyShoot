aa8b05745f497fd3f0a560735075aafeef899742
sakkiyavya <120128233+sakkiyavya@users.noreply.github.com>
2025-11-11 19:09:07 +0800

    1

--- Assets/UI/Menu/Runtime/Menu.cs
+++ Assets/UI/Menu/Runtime/Menu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Menu : MonoBehaviour 
@@ -8,4 +9,17 @@ public class Menu : MonoBehaviour
     public Vector3 hidePos;
     public AnimationCurve bezierCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     public float disappeareTime = 1f;
+    public RectTransform rTran;
+
+    protected virtual void Awake()
+    {
+        rTran = GetComponent<RectTransform>();
+    }
+    public virtual void ShowAndHide(float p)
+    {
+        if (rTran)
+            rTran.localPosition = hidePos * (1 - bezierCurve.Evaluate(p)) + showPos * bezierCurve.Evaluate(p);
+        else
+            Debug.Log(name + "rTrab²»´æÔÚ"); 
+    }
 }
--- Assets/UI/Menu/Runtime/UIManager.cs
+++ Assets/UI/Menu/Runtime/UIManager.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
+
+    public GameObject gameStart;
+    public GameObject gameSetting;
+    public GameObject exit;
+
     Stack<GameObject> UIs = new Stack<GameObject>();
 
     private void Start()
@@ -16,12 +21,28 @@ public class UIManager : MonoBehaviour
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        OpenBaseMenu();
     }
 
     public void OpenUI(GameObject UI)
     {
         UIAppeare(UI);
         UIs.Push(UI);
+    }
+    public void UIAppeare(GameObject UI)
+    {
+        StartCoroutine(IUIAppeare(UI));
+    }
+    IEnumerator IUIAppeare(GameObject UI)
+    {
+        UI.SetActive(true);
+        float t = 0;
+        while(t < UI.GetComponent<Menu>().disappeareTime)
+        {
+            t += Time.deltaTime;
+            UI.GetComponent<Menu>().ShowAndHide(t / UI.GetComponent<Menu>().disappeareTime);
+            yield return null;
+        }
     }
 
     public void CloseUI()
@@ -32,19 +53,30 @@ public class UIManager : MonoBehaviour
             obj.SetActive(false);
         }
     }
-
-    public void UIAppeare(GameObject UI)
-    {
-        UI.SetActive(true);
-    }
-
     public void UIDisappeare(GameObject UI)
     {
-        UI.SetActive(false);
+        StartCoroutine(IUIDisappeare(UI));
+    }
+    IEnumerator IUIDisappeare(GameObject UI)
+    {
+        float t = UI.GetComponent<Menu>().disappeareTime;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            t = Mathf.Max(0, t);
+            UI.GetComponent<Menu>().ShowAndHide(t / UI.GetComponent<Menu>().disappeareTime);
+            yield return null;
+        }
+        UI.SetActive (false);
     }
 
-    IEnumerator IUIAppeare()
+    public void OpenBaseMenu()
     {
-        yield return null;
+        if (gameStart)
+            OpenUI(gameStart);
+        if (gameSetting)
+            OpenUI(gameSetting);
+        if (exit)
+            OpenUI(exit);
     }
 }
